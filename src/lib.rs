51804e7796f098079c48d21fa305154d288daacb
Elijah Morgan <161932463+Scooterking38@users.noreply.github.com>
2026-04-20 13:12:55 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use windows_sys::Win32::UI::WindowsAndMessaging::MessageBoxA;
+
 #[no_mangle]
 pub extern "C" fn hello() {
-    println!("Hello from Rust DLL");
+    unsafe {
+        MessageBoxA(0, b"Hello\0".as_ptr(), b"Rust DLL\0".as_ptr(), 0);
+    }
 }
